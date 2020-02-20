bed5116c6aa81fe9ce9f0f9a419e15a2ce54a85c
Mariana Carvalho Mello Cesar <mccesar@stefanini.com>
2020-02-20 18:51:49 -0300

    Minor correction in booking controller. Typo found.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -64,7 +64,7 @@ exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
-    event = stripe.webhooks.construsEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
